1fbdb36858673de35557e965a075f18e60729e2e
Riccardo Di Sipio <riccardo@ideal.com>
2021-04-13 14:56:40 +0000

    jax

--- scripts/train_haiku_model.py
+++ scripts/train_haiku_model.py
@@ -20,6 +20,10 @@ def get_ngrams(X, n):
     return ngrams
 
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+   tf.config.experimental.set_memory_growth(gpu, True)
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
